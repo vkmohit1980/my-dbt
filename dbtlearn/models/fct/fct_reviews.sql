6c91adf91340f77bb399e0196f9b9c68c8ba50d9
VijayKMohit <v.k.mohit@gmail.com>
2024-02-15 15:14:31 +0000

    Fixed fct_reviews error

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -13,6 +13,7 @@ WITH src_reviews AS (
 SELECT 
   {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
   *
+FROM src_reviews    
 WHERE review_text IS NOT NULL
   {% if is_incremental() %}
     AND review_date > (SELECT MAX(review_date) FROM {{ this }})
